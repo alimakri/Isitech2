8da503c0f7ea5eecb5525444845e6dd163e6caee
Ali MAKRI <github@makri.fr>
2023-03-06 12:38:28 +0100

    12:38

--- Script/2.sql
+++ Script/2.sql
@@ -36,3 +36,40 @@ ALTER TABLE [dbo].[Personne] CHECK CONSTRAINT [FK_Personne_Ville]
 GO
 
 
+INSERT Ville (Libelle) values ('Lyon'), ('Marseille'), ('Paris')
+select * from Ville
+
+INSERT Personne(Libelle, Ville) values 
+			('Pierre', 3),
+			('Martine', 2),
+			('Laurent', 1)
+
+select * from Personne
+
+SELECT Personne.Libelle AS NomPersonne, Ville.Libelle AS NomVille
+FROM     Personne INNER JOIN
+                  Ville ON Personne.Ville = Ville.Id
+
+use AdventureWorks2017;
+
+SELECT 
+	Person.Person.BusinessEntityID, 
+	Person.Person.FirstName, 
+	Person.Person.LastName, 
+	Person.Address.City
+FROM     
+	Person.Person 
+	INNER JOIN Person.BusinessEntity ON Person.Person.BusinessEntityID = Person.BusinessEntity.BusinessEntityID 
+	INNER JOIN Person.BusinessEntityAddress ON Person.BusinessEntity.BusinessEntityID = Person.BusinessEntityAddress.BusinessEntityID 
+	INNER JOIN Person.Address ON Person.BusinessEntityAddress.AddressID = Person.Address.AddressID
+
+SELECT Production.Product.ProductID, Production.Product.Name, Production.ProductSubcategory.Name AS Expr1, Production.ProductCategory.Name AS Expr2
+FROM     Production.Product INNER JOIN
+                  Production.ProductSubcategory ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID INNER JOIN
+                  Production.ProductCategory ON Production.ProductSubcategory.ProductCategoryID = Production.ProductCategory.ProductCategoryID
+use BD1;
+select * from Ville
+delete Ville where id=3
+select * from Personne
+
+delete Personne where id=1
