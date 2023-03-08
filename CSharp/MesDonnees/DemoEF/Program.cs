c2796e76b933179d3c84f37e25245c42c7f5303b
Ali MAKRI <github@makri.fr>
2023-03-08 15:06:29 +0100

    15:06

--- CSharp/MesDonnees/DemoEF/Program.cs
+++ CSharp/MesDonnees/DemoEF/Program.cs
@@ -10,6 +10,14 @@ namespace DemoEF
     {
         static void Main(string[] args)
         {
+            var context = new AdvContext();
+            var tousLesProduits = context.Products;
+            var tousLesVelos = context.Products
+                .Where(x => x.ProductSubcategory.ProductCategory.Name == "Bikes")
+                .ToList();
+
+            tousLesVelos.ForEach(velo => velo.ListPrice /= 1.1M);
+            context.SaveChanges();
         }
     }
 }
---
+++ CSharp/MesDonnees/DemoLinq/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLinq
+{
+    class Animal
+    {
+        public string Nom;
+        public DateTime DateNaissance;
+        public override string ToString()
+        {
+            return Nom;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var resultat = VersionPrimitive();
+            // var resultat = VersionLinq();
+            var resultat = VersionLinqAvance();
+
+            // Affichage du résultat
+            foreach (var i in resultat)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+        private static List<string> VersionLinqAvance()
+        {
+            var animaux = new List<Animal>
+            {
+                new Animal{ Nom="Medor", DateNaissance= DateTime.Now},
+                new Animal{ Nom="Titi", DateNaissance= DateTime.Now.AddDays(-1)},
+                new Animal{ Nom="GrosMinet", DateNaissance= DateTime.Now.AddDays(-2)},
+                new Animal{ Nom="Minou", DateNaissance= DateTime.Now.AddDays(-3)},
+                new Animal{ Nom="Toto", DateNaissance= DateTime.Now.AddDays(-2)},
+            };
+
+            return animaux
+                        .Where(x => (int)((DateTime.Now - x.DateNaissance).TotalDays) == 2)
+                        .Select(x => x.Nom)
+                        .ToList();
+
+        }
+        private static List<int> VersionLinq()
+        {
+            var tableauEntier = new int[] { 12, 58, 39, 2, 7, 4, 154, 11 };
+
+            // Corps
+            var resultat = tableauEntier
+                                .Where(x => x % 2 == 1)
+                                .Where(x => x > 10)
+                                .ToList();
+
+            return resultat;
+        }
+
+        private static List<int> VersionPrimitive()
+        {
+            var tableauEntier = new int[] { 12, 58, 39, 2, 7, 4, 154, 11 };
+
+            // Corps
+            List<int> resultat = new List<int>();
+            foreach (var i in tableauEntier)
+            {
+                if (i % 2 == 1) resultat.Add(i);
+            }
+
+            return resultat;
+        }
+    }
+}
